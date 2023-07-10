13d5dc47aba485cd166f1e916fca80418cc4818e
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2023-07-10 23:16:42 +0530

    Update Q_5.scala

--- Q_5.scala
+++ Q_5.scala
@@ -2,7 +2,7 @@ object Q_5 {
   def main(args: Array[String]): Unit = {
     print("Enter your number - ")
     val number = scala.io.StdIn.readInt()
-    print(s"The addition of all even numbers less than given ${number} is " +(sum(number) - number))
+    print(s"The addition of all even numbers less than given ${number} is " +sum(number))
   }
 
   def sum(number: Int): Int = {
